c9378e3f892bc1bac9985eda9f7b451a52c2c54e
Ahmed Abd Elaziz <aaziz@Elkotb.local>
2026-01-11 10:26:40 +0200

    ver 3

--- src/components/MusicSection.jsx
+++ src/components/MusicSection.jsx
@@ -1,10 +1,10 @@
-import { FlashTimer } from "../utility";
-import JBL from "../assets/JBL.png";
-import { useTranslation } from "react-i18next";
+import { FlashTimer } from '../utility'
+import JBL from '../assets/JBL.png'
+import { useTranslation } from 'react-i18next'
 // import JBL from "../assets/JBL_BOOMBOX_2_HERO_020_x1 (1) 1 (1).png";
 const MusicSection = () => {
-  const { seconds, minutes, hours, days } = FlashTimer();
-  const { t } = useTranslation();
+  const { seconds, minutes, hours, days } = FlashTimer()
+  const { t } = useTranslation()
 
   return (
     <section className="bg-black grid grid-cols-1 md:grid-cols-2 min-h-96 text-white px-6 md:px-12 py-8 md:py-14 my-10">
@@ -12,35 +12,37 @@ const MusicSection = () => {
       <div className="flex gap-y-5 flex-col justify-between md:justify-start items-start  ">
         <p className="capitalize text-green-500">categories</p>
         <h1 className="text-3xl md:text-4xl my-5 lg:text-5xl font-semibold ">
-          {t("Enhance Your Music Experience")}
+          {t('Enhance Your Music Experience')}
         </h1>
         {/* timer */}
-        <div className="text-black flex gap-5 items-center">
-          <div className="rounded-full bg-white w-20 h-20 flex gap-2 flex-col justify-center items-center  ">
-            <p className="font-bold text-xl">{String(days).padStart(2, "0")}</p>
-            <p className="text-sm capitalize">{t("days")}</p>
-          </div>
-          <div className="rounded-full bg-white w-20 h-20 flex gap-2 flex-col justify-center items-center  ">
-            <p className="font-bold text-xl">
-              {String(hours).padStart(2, "0")}
+        <div className="text-black flex gap-3 md:gap-5 items-center">
+          <div className="rounded-full bg-white w-13 h-13 md:w-20 md:h-20 flex gap-2 flex-col justify-center items-center  ">
+            <p className="font-bold text-sm md:text-xl">
+              {String(days).padStart(2, '0')}
             </p>
-            <p className="text-sm capitalize">{t("hours")}</p>
+            <p className="text-xs md:text-sm capitalize">{t('days')}</p>
           </div>
-          <div className="rounded-full bg-white w-20 h-20 flex gap-2 flex-col justify-center items-center  ">
-            <p className="font-bold text-xl">
-              {String(minutes).padStart(2, "0")}
+          <div className="rounded-full bg-white w-13 h-13 md:w-20 md:h-20 flex gap-2 flex-col justify-center items-center  ">
+            <p className="font-bold text-sm md:text-xl">
+              {String(hours).padStart(2, '0')}
             </p>
-            <p className="text-sm capitalize">{t("minutes")}</p>
+            <p className="text-xs md:text-sm capitalize">{t('hours')}</p>
           </div>
-          <div className="rounded-full bg-white w-20 h-20 flex gap-2 flex-col justify-center items-center  ">
-            <p className="font-bold text-xl">
-              {String(seconds).padStart(2, "0")}
+          <div className="rounded-full bg-white w-13 h-13 md:w-20 md:h-20 flex gap-2 flex-col justify-center items-center  ">
+            <p className="font-bold text-sm md:text-xl">
+              {String(minutes).padStart(2, '0')}
             </p>
-            <p className="text-sm capitalize">{t("seconds")}</p>
+            <p className="text-xs md:text-sm capitalize">{t('minutes')}</p>
+          </div>
+          <div className="rounded-full bg-white w-13 h-13 md:w-20 md:h-20 flex gap-2 flex-col justify-center items-center  ">
+            <p className="font-bold text-sm md:text-xl">
+              {String(seconds).padStart(2, '0')}
+            </p>
+            <p className="text-xs md:text-sm capitalize">{t('seconds')}</p>
           </div>
         </div>
         <button className="btn bg-green-500 px-7 py-3 rounded capitalize cursor-pointer hover:bg-green-600 transition duration-300">
-          {t("buy now")}
+          {t('buy now')}
         </button>
       </div>
       {/* image */}
@@ -52,7 +54,7 @@ const MusicSection = () => {
         />
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default MusicSection;
+export default MusicSection
